a4980a34d4860e7638cf942714c3cf5cbc69575e
Jeancasoto <jeancasoto@yahoo.com>
2017-10-20 14:23:09 -0600

    Empeze alfin

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int opcion;
+
+cout<<"Bienvenido acontinuacion seleccione una opcion"<<endl;
+cout<<"Ejercicio #1"<<endl;
+cout<<"Ejercicio #2"<<endl;
+cout<<"Ejercicio #3"<<endl;
+cin>>opcion;
+
+switch(){
+case 1: {
+
+cout<<"Elijio el caso 1"<<endl;
+
+}//fin case 1
+break;
+case 2: {
+
+cout<<"Elijio el caso 2"<<endl;
+
+
+
+}//fin case 2
+break
+case 3:{
+
+cout<<"Elijio el caso 3"<<endl;
+
+}//fin case 3
+break;
+
+
+
+}//fin switch
+
+
+
+
+}
